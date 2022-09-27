2dbce9d8f5815a46723a11d2c0ced58d61d2b15a
15727684438 <15727684438@qq.com>
2022-09-27 21:09:33 +0800

    0

---
+++ 主函数变量和传递的参数变量地址不同.c
@@ -0,0 +1,13 @@
+//#include <stdio.h>
+//void PRINT(int, int* a);
+//int		 main(void) {
+//	int a = 0x1;
+//	int* address = &a;
+//	PRINT(a, address);
+//	printf("1:%p\n", &a);
+//}			
+//
+//void PRINT(int b, int* a) {
+//	printf("2:%p\n", &b);
+//	printf("%zd", &b - a);
+//}
---
+++ 二维数组指针使用浅例.c
@@ -0,0 +1,25 @@
+////目标:定义一个二维数组,并加和
+//#include <stdio.h>
+//#define COLS 3
+//#define ROWS 2
+//int sum2d(int ar[][COLS], int rows);
+//int main(void) {
+//	const int array_2[2][3] = { 1,1,1,1,1,1};
+//	int i, j;
+//	int total = 0;
+//	total=sum2d(array_2,ROWS);
+//	printf("%d",total);
+//	return 0;
+//}
+//int sum2d(int ar[][COLS], int rows)//二维数组求和函数
+//{
+//    int r;
+//    int c;
+//    int tot = 0;
+//
+//    for (r = 0; r < rows; r++)
+//        for (c = 0; c < COLS; c++)
+//            tot += ar[r][c];
+//
+//    return tot;
+//}
