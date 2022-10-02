4d95c6eec20957733ce2e0fc5892428f28b10000
1745180072 <1745180072@qq.com>
2022-10-02 16:46:35 +0800

    2312

---
+++ 字符串是静态存储类型.c
@@ -0,0 +1,14 @@
+//#include <stdio.h>
+//void strings(void);
+//int main(void) {
+//	for (size_t i = 0; i < 10; i++)
+//	{
+//		strings();
+//	}
+//	getchar();
+//	return 0;
+//}
+//void strings(void) {
+//	char a[] = "来看待世界flak精神";
+//	printf("num1:%p num2:%p  %td\n  ", a, &a, a - &a);
+//}
