181601fcb26ed74d7f3a2c3ee806bdbe4db67388
1745180072 <1745180072@qq.com>
2022-10-02 16:46:35 +0800

    012312

--- const初探.c
+++ const初探.c
@@ -1,13 +1,13 @@
-//指针修改了const数组(雾)
-#include <stdio.h>	
-int main(void) {
-	 const int ar[3] = { 1,2,3 };
-	int* pt1 = &ar, *pt2=&ar, *pt3=&ar;
-	printf("*pt1++=%d\n",*pt1++);
-	printf("(*pt2)=%d\n", (*pt2)++);
-	printf("*(pt3++)=%d\n",*(pt3++));
-	for (size_t i = 0; i < 3; i++)
-	{
-		printf("%d\n", ar[i]);
-	}
-}
+////指针修改了const数组(雾)
+//#include <stdio.h>
+//int main(void) {
+//	 const int ar[3] = { 1,2,3 };
+//	int* pt1 = &ar, *pt2=&ar, *pt3=&ar;
+//	printf("*pt1++=%d\n",*pt1++);
+//	printf("(*pt2)=%d\n", (*pt2)++);
+//	printf("*(pt3++)=%d\n",*(pt3++));
+//	for (size_t i = 0; i < 3; i++)
+//	{
+//		printf("%d\n", ar[i]);
+//	}
+//}
