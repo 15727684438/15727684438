a77466850159e66d3e8f6971e41a9357e4cb94d5
15727684438 <15727684438@qq.com>
2022-10-06 19:59:36 +0800

    2

--- const初探.c
+++ const初探.c
@@ -1,13 +1,11 @@
-////指针修改了const数组(雾)
-//#include <stdio.h>
-//int main(void) {
-//	 const int ar[3] = { 1,2,3 };
-//	int* pt1 = &ar, *pt2=&ar, *pt3=&ar;
-//	printf("*pt1++=%d\n",*pt1++);
-//	printf("(*pt2)=%d\n", (*pt2)++);
-//	printf("*(pt3++)=%d\n",*(pt3++));
-//	for (size_t i = 0; i < 3; i++)
-//	{
-//		printf("%d\n", ar[i]);
-//	}
-//}
+//指针修改了const数组(雾)
+#include <stdio.h>
+int main(void) {
+	 char* ar= "abc";
+	 //printf("*pt1++=%d\n",*pt1++);++优先级更高
+	/*for (size_t i = 0; i < 3; i++)
+	{
+		printf("%d\n", ar[i]);
+	}*/	
+	 putchar(*ar++);
+}
---
+++ fgets.c
@@ -0,0 +1,10 @@
+//#include <stdio.h>
+//#include "mylibrary.h"
+//int main(void) {
+//	char a[27];
+//	printf("%p", fgets(a, 25, stdin));
+//	
+//	printf("\n%s", a);//fgets读入n-1个字符,并返回存入的数组地址(也许不一定是数组)
+//	clear_buf();
+//	getchar();
+//}
