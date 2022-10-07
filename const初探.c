e342377f30cbf5528a917c39dcf03e37e9d0371c
15727684438 <15727684438@qq.com>
2022-10-07 16:00:43 +0800

    1

--- const初探.c
+++ const初探.c
@@ -1,12 +1,13 @@
 //指针修改了const数组(雾)
 #include <stdio.h>
 int main(void) {
-	 char* ar= 0x007069550;
+	 char* ar= "213123123";
 	 //printf("*pt1++=%d\n",*pt1++);++优先级更高
 	/*for (size_t i = 0; i < 3; i++)
 	{
 		printf("%d\n", ar[i]);
 	}*/	
-	while(1) putchar(*ar++);
+	while(11) putchar(*ar++);//非0即为真
+
 	getchar();
 }
---
+++ mylibrary.h
@@ -0,0 +1,3 @@
+void clear_buf() {//清除输入缓冲区的剩余内容
+	while (getchar() != '\n') continue;
+};
