b2bde98c0e96b0e1b4b3a2b2e14ed5af8469e62c
15727684438 <15727684438@qq.com>
2022-10-07 11:31:21 +0800

    1

--- const初探.c
+++ const初探.c
@@ -1,11 +1,12 @@
 //指针修改了const数组(雾)
 #include <stdio.h>
 int main(void) {
-	 char* ar= "abc";
+	 char* ar= 0x007069550;
 	 //printf("*pt1++=%d\n",*pt1++);++优先级更高
 	/*for (size_t i = 0; i < 3; i++)
 	{
 		printf("%d\n", ar[i]);
 	}*/	
-	 putchar(*ar++);
+	while(1) putchar(*ar++);
+	getchar();
 }
