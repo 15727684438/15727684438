6d85c2a76e01f306318018dabdae9ac568fa38c2
15727684438 <15727684438@qq.com>
2022-10-07 21:04:32 +0800

    1

---
+++ strcat测试.c
@@ -0,0 +1,8 @@
+//#include <stdio.h>
+//#include <string.h>
+//int main(void) {
+//	char dst[6]={'a','b','c','d','e','\0'};
+//	strcat(dst, "eat wo yichu gongji");
+//	puts(dst);
+//	return 0;
+//}//Òç³ö¹¥»÷³É¹¦
