5a7205b97c55ba22141c01714f84ca7357aa6d7f
15727684438 <15727684438@qq.com>
2022-10-07 20:52:50 +0800

    1

--- const初探.c
+++ const初探.c
@@ -1,13 +1,11 @@
-//指针修改了const数组(雾)
-#include <stdio.h>
-int main(void) {
-	 char* ar= "213123123";
-	 //printf("*pt1++=%d\n",*pt1++);++优先级更高
-	/*for (size_t i = 0; i < 3; i++)
-	{
-		printf("%d\n", ar[i]);
-	}*/	
-	while(11) putchar(*ar++);//非0即为真
-
-	getchar();
-}
+////指针修改了const数组(雾)
+//#include <stdio.h>
+//#include <string.h>
+//int main(void) {
+//	 //printf("*pt1++=%d\n",*pt1++);++优先级更高
+//	/*for (size_t i = 0; i < 3; i++)
+//	{
+//		printf("%d\n", ar[i]);
+//	}*/	
+//	 printf("%d",strcmp("z","b"));//vs的strcmp函数只返回0,-1,1
+//}
---
+++ 赋值表达式的结果.c
@@ -0,0 +1,8 @@
+//#include <stdio.h>
+//int main(void) {
+//	int a=6;
+//	printf("%d",a=5);
+//	
+//	return 0;
+//}
+////结果是左值
