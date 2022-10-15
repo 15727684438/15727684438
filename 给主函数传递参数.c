82f2c68c7f67f317dde9339a1da2cf6e90bdd723
15727684438 <15727684438@qq.com>
2022-10-15 23:06:16 +0800

    1

--- 给主函数传递参数.c
+++ 给主函数传递参数.c
@@ -1,40 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#define LENGTH 20
-void get_arg(char* a);
-
-int main() {
-	int flag = 0;//功能选择 1关机 2待定
-	char cs[LENGTH] ={0};
-	printf("请输入功能参数\n1关机 2待定\n ");
-	scanf("%d",&flag);
-	getchar();
-	if (flag=1)
-	{
-		char a[LENGTH+10] = "shutdown ";
-		printf("输入参数:\n");
-		gets_s(cs, LENGTH);
-		//scanf("%s",&参数);
-		strcat(a, cs);
-		a[29] = '0';
-		system(a);
-	}
-	return 0;
- 	}	
-//void get_arg(char* a) {
-//	
-//	for (int i = 0;i<LENGTH; i++, a++)
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#define LENGTH 20
+//void get_arg(char* a);
+//
+//int main() {
+//	int flag = 0;//功能选择 1关机 2待定
+//	char cs[LENGTH] ={0};
+//	printf("请输入功能参数\n1关机 2待定\n ");
+//	scanf("%d",&flag);
+//	getchar();
+//	if (flag=1)
 //	{
-//		if (*a = getchar() && *a != '\n'&& *a !='0')
-//		continue; 
-//		
-//		else { 
-//			*a = '\0';
-//
-//			break; 
-//		}
-//		
-//
+//		char a[LENGTH+10] = "shutdown ";
+//		printf("输入参数:\n");
+//		gets_s(cs, LENGTH);
+//		//scanf("%s",&参数);
+//		strcat(a, cs);
+//		a[29] = '0';
+//		system(a);
 //	}
-//	}
+//	return 0;
+// 	}	
+////void get_arg(char* a) {
+////	
+////	for (int i = 0;i<LENGTH; i++, a++)
+////	{
+////		if (*a = getchar() && *a != '\n'&& *a !='0')
+////		continue; 
+////		
+////		else { 
+////			*a = '\0';
+////
+////			break; 
+////		}
+////		
+////
+////	}
+////	}
